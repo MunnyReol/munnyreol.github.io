85453b436ca6ee83e46080565d9ba7b05980351c
MunnyReol <59450380+MunnyReol@users.noreply.github.com>
2020-03-26 10:23:57 +0530

    Update main.js

--- twitter-live-follower/assets/js/main.js
+++ twitter-live-follower/assets/js/main.js
@@ -1,9 +1,9 @@
 $(document ).ready(function (){
-
-function getRandomName() { var names = [ "munnyreol", "BarackObama","JustinBieber", "AmitabhBachan" ]; return names[Math.floor(Math.random() * names.length)];} 
- 
-var id= prompt("Enter your favorite Twitter channel Name",getRandomName())
-$("button").click(function (){
+if(id="0"){
+    id="BarackObama"
+} else{
+    id=idd
+}
 setInterval(function() {
 $.getJSON("https://api.thesocialcounter.com/twitter/?name="+id, function (data) {
 
@@ -15,7 +15,29 @@ $.getJSON("https://api.thesocialcounter.com/twitter/?name="+id, function (data) 
         $(".title").text(title);
         $("#odometer").text(flwr);
         
-        console.log("Flower="+flwr)
+        
+         var image = document.querySelector('#user_pic'); image.src = pic;
+          var image = document.querySelector('#bckgrnd'); image.src = bckgrnd;
+         
+        })
+        }, 2500); 
+$("button").click(function (){
+function getRandomName() { var names = [ "munnyreol", "BarackObama","JustinBieber", "AmitabhBachan", "iamsrk"]; return names[Math.floor(Math.random() * names.length)];} 
+ 
+var idd= id
+id=prompt("Enter your favorite Twitter channel Name",getRandomName())
+
+setInterval(function() {
+$.getJSON("https://api.thesocialcounter.com/twitter/?name="+id, function (data) {
+
+        flwr = data.followers_count;
+        title=data.name;
+        pic=data.profile_image_url;
+        bckgrnd=data.profile_banner_url;
+        
+        $(".title").text(title);
+        $("#odometer").text(flwr);
+        
         
          var image = document.querySelector('#user_pic'); image.src = pic;
           var image = document.querySelector('#bckgrnd'); image.src = bckgrnd;
