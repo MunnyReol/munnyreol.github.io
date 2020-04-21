c3059807441c0572da3fe9a12f4d9598b6279b1a
Monirul Islam <59450380+MunnyReol@users.noreply.github.com>
2020-04-22 01:14:15 +0530

    Update main.js

--- twitter-live-follower/assets/js/main.js
+++ twitter-live-follower/assets/js/main.js
@@ -34,7 +34,7 @@ var idd= id
 id=prompt("Enter your favorite Twitter channel Name",getRandomName())
 
 setInterval(function() {
-$.getJSON("https://api.thesocialcounter.com/twitter/?name="+id, function (data) {
+$.getJSON("https://cors-anywhere.herokuapp.com/https://mlmcounts.herokuapp.com/twit/api/?name="+id, function (data) {
 
         flwr = data.followers_count;
         title=data.name;
