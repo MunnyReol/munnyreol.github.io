4ee0a6990aae7775c417248095ec7a3e7fdf4240
Monirul Islam <59450380+MunnyReol@users.noreply.github.com>
2020-05-23 15:46:58 +0530

    Highchart Add

--- twitter-live-follower/assets/js/main.js
+++ twitter-live-follower/assets/js/main.js
@@ -5,7 +5,75 @@ if(id ==null){
 id="BarackObama"
 window.location.href = "?user="+id
 }
-//replaceid= id.replace(/%20/gi, "");
+
+
+
+
+
+var lineColor = "red"
+var textBright = "red"
+$(function asd(){
+    
+var chart = new Highcharts.chart({
+              chart: {
+                  renderTo: 'chart',
+                  type: 'spline',
+                    zoomType: 'x',
+                    backgroundColor: 'transparent',
+                    plotBorderColor: 'transparent'
+              },
+              title: {
+                  text: 'Live Chart HighChart'
+              },
+              xAxis: {
+                  type: 'datetime',
+                  tickPixelInterval: 200,
+                    gridLineColor: lineColor,
+                    labels: {
+                        style: {
+                            color: textBright
+                        }
+                    },
+                    lineColor: lineColor,
+                    minorGridLineColor: 'red',
+                    tickColor: lineColor,
+                    title: {
+                        style: {
+                            color: textBright
+                        }
+                    }
+              },
+              yAxis: {
+                  title: {
+                      text: ''
+                  },
+                    gridLineColor: lineColor,
+                    labels: {
+                        style: {
+                            color: textBright
+                        }
+                    },
+                    lineColor: lineColor,
+                    minorGridLineColor: 'red',
+                    tickColor: lineColor
+              },
+              credits: {
+                  enabled: false
+              },
+
+              series: [{
+                 name: 'Followers',
+                marker: { enabled: false },
+                color: '#17D92D',
+                lineColor: '#DA1DAF'
+              }]
+            });
+
+
+
+
+
+
 
 $.getJSON("https://cors-munnyreol.herokuapp.com/http://munnyreol.herokuapp.com/twitter/api/?user="+/*replace*/id, function (data) {
 /*https://cors-anywhere.herokuapp.com/https://api.thesocialcounter.com/twitter/?name=*/
@@ -16,6 +84,11 @@ $.getJSON("https://cors-munnyreol.herokuapp.com/http://munnyreol.herokuapp.com/t
 
         $(".title").text(title);
         $("#odometer").text(flwr);
+
+chart.series[0].addPoint([                   
+                (new Date()).getTime(),
+                Math.abs(flwr)
+                ])
         
         
          var image = document.querySelector('#user_pic'); image.src = pic;
@@ -27,8 +100,14 @@ $.getJSON("https://cors-munnyreol.herokuapp.com/http://munnyreol.herokuapp.com/t
 $.getJSON("https://cors-munnyreol.herokuapp.com/http://munnyreol.herokuapp.com/twitter/api/?user="+/*replace*/id, function (data) {
         flwr = data.followers;
         $("#odometer").text(flwr);
+
+chart.series[0].addPoint([                   
+                (new Date()).getTime(),
+                Math.abs(flwr)
+                ])
         }); 
          }, 2500)
+})
 
 $("button").click(function (){
 function getRandomName() {
